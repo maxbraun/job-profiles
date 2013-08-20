ccdd3d925966c4b5dd83fd6432883d6c970984aa
Maximilian Braun <maximilian.braun@1und1.de>
2013-08-20 18:48:48 +0200

    riesiges foo

---
+++ src/main/java/org/jenkinsci/plugins/jobprofiles/JobProfileProperties.java
@@ -0,0 +1,11 @@
+package org.jenkinsci.plugins.jobprofiles;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mabraun
+ * Date: 20.08.13
+ * Time: 18:19
+ * To change this template use File | Settings | File Templates.
+ */
+public class JobProfileProperties {
+}
