af04b0218fc6f25eb490ffba377a934d44a669a4
Maximilian Braun <maximilian.braun@1und1.de>
2013-08-21 09:12:14 +0200

    Properties for a Project, i want to savem some information to get information reverse

--- src/main/java/org/jenkinsci/plugins/jobprofiles/JobProfileProperties.java
+++ src/main/java/org/jenkinsci/plugins/jobprofiles/JobProfileProperties.java
@@ -1,11 +1,32 @@
 package org.jenkinsci.plugins.jobprofiles;
 
-/**
- * Created with IntelliJ IDEA.
- * User: mabraun
- * Date: 20.08.13
- * Time: 18:19
- * To change this template use File | Settings | File Templates.
- */
-public class JobProfileProperties {
+
+import hudson.Extension;
+import hudson.model.AbstractProject;
+import hudson.tasks.BuildWrapper;
+import hudson.tasks.BuildWrapperDescriptor;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import java.util.Map;
+
+public class JobProfileProperties  extends BuildWrapper{
+    public final Map<String, String> properties;
+
+    @DataBoundConstructor
+    public JobProfileProperties(Map<String, String> properties) {
+        this.properties = properties;
+    }
+
+    @Extension
+    public static class DescriptorImpl extends BuildWrapperDescriptor {
+        @Override
+        public boolean isApplicable(AbstractProject<?, ?> item) {
+            return true;
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "Job Profile Informations";
+        }
+    }
 }
