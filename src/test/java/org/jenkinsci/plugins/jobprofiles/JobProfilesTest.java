1316ae1c2f2822622a20d6ef28d18f1a6d8cf723
Maximilian Braun <maximilian.braun@1und1.de>
2013-09-16 09:32:43 +0200

    added test config

--- src/test/java/org/jenkinsci/plugins/jobprofiles/JobProfilesTest.java
+++ src/test/java/org/jenkinsci/plugins/jobprofiles/JobProfilesTest.java
@@ -1,21 +1,24 @@
 package org.jenkinsci.plugins.jobprofiles;
 
-import hudson.Launcher;
-import hudson.model.*;
+import hudson.model.FreeStyleBuild;
+import hudson.model.FreeStyleProject;
+import hudson.model.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
-import org.jvnet.hudson.test.TestBuilder;
-
-import java.io.IOException;
 
 @Slf4j
 public class JobProfilesTest {
-    @Rule public JenkinsRule j = new JenkinsRule();
-    @Test public void systemJobs() throws Exception {
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
+
+    @Test
+    public void systemJobs() throws Exception {
         FreeStyleProject project = j.createFreeStyleProject();
+        JobProfilesConfiguration.get().setSoftwareIndexFile("svn:https://github.com/maxbraun/job-profiles/trunk/src/main/resources/softreg.xml");
+        JobProfilesConfiguration.get().setProfileRootDir("https://github.com/maxbraun/job-profiles-examles.git");
         project.getBuildersList().add(new JobProfiles("system", "system"));
         FreeStyleBuild build = project.scheduleBuild2(0).get();
 
@@ -25,8 +28,11 @@ public class JobProfilesTest {
 
     }
 
-    @Test public void NormalJobs() throws Exception {
+    @Test
+    public void NormalJobs() throws Exception {
         j.configureMaven3();
+        JobProfilesConfiguration.get().setSoftwareIndexFile("svn:https://github.com/maxbraun/job-profiles/trunk/src/main/resources/softreg.xml");
+        JobProfilesConfiguration.get().setProfileRootDir("https://github.com/maxbraun/job-profiles-examles.git");
         FreeStyleProject project = j.createFreeStyleProject();
         project.getBuildersList().add(new JobProfiles("", ""));
         FreeStyleBuild build = project.scheduleBuild2(0).get();
