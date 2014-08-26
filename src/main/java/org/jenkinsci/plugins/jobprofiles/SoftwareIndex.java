1897c8c4f5828942a71baa69e28fa2d7dcebf1f4
Maximilian braun <code@max-braun.com>
2014-08-26 17:12:55 +0200

    formatting

--- src/main/java/org/jenkinsci/plugins/jobprofiles/SoftwareIndex.java
+++ src/main/java/org/jenkinsci/plugins/jobprofiles/SoftwareIndex.java
@@ -1,20 +1,20 @@
 package org.jenkinsci.plugins.jobprofiles;
 
 
-import com.thoughtworks.xstream.annotations.XStreamAlias;
-import com.thoughtworks.xstream.annotations.XStreamImplicit;
-import com.thoughtworks.xstream.io.xml.DomDriver;
-import hudson.util.XStream2;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-import lombok.extern.slf4j.Slf4j;
-import net.oneandone.sushi.fs.*;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamImplicit;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+
+import hudson.util.XStream2;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.oneandone.sushi.fs.Node;
 
 @Slf4j
 @Data
